eb84d693a5dd8497c3ef3618ac8845af2e4101de
Willinthon Carriedo <anthonycarriedo2013@gmail.com>
2020-04-10 00:58:11 -0300

    fronten

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-async function main() {
-    await app.listen(app.get('port'));
-    console.log('Server on port', app.get('port'))
-}
-
-main();
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  domain: 'localhost'
+  domain: 'https://restaurantadminbackend.herokuapp.com'
 };
 
 /*
